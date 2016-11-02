61225d649b32d5abca57bb809c1a1e48e5e16574
AAMARAL\aa <alag14@gmail.com>
2016-11-02 14:43:58 -0300

    Se agrega proyecto con Entidades para las funciones del web services.Se agrega proyecto con el Web services ya las funciones que seran utilizadas desde el Winform.

--- Formularios/WebServices/WebService.asmx.cs
+++ Formularios/WebServices/WebService.asmx.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using System.Collections;
+using Entidades;
 
 namespace WebServices
 {
@@ -18,9 +20,64 @@ namespace WebServices
     {
 
         [WebMethod]
-        public string HelloWorld()
+        public bool RegistrarUsuario(Usuario usuario)
         {
-            return "Hello World";
+            return true;
         }
+
+        [WebMethod]
+        public bool ActualizarUsuario(Usuario usuario)
+        {
+            return true;
+        }
+
+
+        [WebMethod]
+        public List<Rol> ListarRoles()
+        {
+            
+            return null;
+        }
+
+        [WebMethod]
+        public bool AsignarRolUsuario(List<Usuario>  listaUsuarios,Rol rol)
+        {
+            return true;
+        }
+
+        [WebMethod]
+        public bool IngresarCliente(Cliente cliente)
+        {
+            return true;
+        }
+
+        [WebMethod]
+        public List<Cliente> ListarClientes()
+        {
+            return null;
+        }
+
+
+        [WebMethod]
+        public bool RegistrarConfiguracion(Configuracion configuracion)
+        {
+            return true;
+        }
+
+        [WebMethod]
+        public bool ActualizarConfiguracion(Configuracion configuracion)
+        {
+            return true;
+        }
+
+        [WebMethod]
+        public List<Configuracion> ListarConfiguraciones()
+        {
+            return null;
+        }
+
+
+
+
     }
 }
