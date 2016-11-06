8eaeb858de563418b5ff238dd3563ff336c7fe71
Pablo <pablocabillon@gmail.com>
2016-11-06 19:27:19 -0300

    Se agregan validaciones

--- obligatorioPuntoNet2016Licencias/Login/Login.Designer.cs
+++ obligatorioPuntoNet2016Licencias/Login/Login.Designer.cs
@@ -28,32 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.components = new System.ComponentModel.Container();
+            this.txbUsuario = new System.Windows.Forms.TextBox();
+            this.txbContraseña = new System.Windows.Forms.TextBox();
             this.lblUsuario = new System.Windows.Forms.Label();
             this.lblContraseña = new System.Windows.Forms.Label();
             this.btnIngresar = new System.Windows.Forms.Button();
             this.btnSalir = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txbUsuario
             // 
-            this.textBox1.Location = new System.Drawing.Point(109, 18);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(134, 20);
-            this.textBox1.TabIndex = 0;
+            this.txbUsuario.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbUsuario.Location = new System.Drawing.Point(119, 27);
+            this.txbUsuario.Name = "txbUsuario";
+            this.txbUsuario.Size = new System.Drawing.Size(156, 20);
+            this.txbUsuario.TabIndex = 0;
             // 
-            // textBox2
+            // txbContraseña
             // 
-            this.textBox2.Location = new System.Drawing.Point(109, 53);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(134, 20);
-            this.textBox2.TabIndex = 1;
+            this.txbContraseña.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txbContraseña.Location = new System.Drawing.Point(119, 62);
+            this.txbContraseña.Name = "txbContraseña";
+            this.txbContraseña.Size = new System.Drawing.Size(156, 20);
+            this.txbContraseña.TabIndex = 1;
             // 
             // lblUsuario
             // 
+            this.lblUsuario.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lblUsuario.AutoSize = true;
-            this.lblUsuario.Location = new System.Drawing.Point(22, 22);
+            this.lblUsuario.Location = new System.Drawing.Point(32, 31);
             this.lblUsuario.Name = "lblUsuario";
             this.lblUsuario.Size = new System.Drawing.Size(59, 13);
             this.lblUsuario.TabIndex = 2;
@@ -61,8 +73,11 @@
             // 
             // lblContraseña
             // 
+            this.lblContraseña.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.lblContraseña.AutoSize = true;
-            this.lblContraseña.Location = new System.Drawing.Point(22, 57);
+            this.lblContraseña.Location = new System.Drawing.Point(32, 66);
             this.lblContraseña.Name = "lblContraseña";
             this.lblContraseña.Size = new System.Drawing.Size(77, 13);
             this.lblContraseña.TabIndex = 3;
@@ -70,7 +85,8 @@
             // 
             // btnIngresar
             // 
-            this.btnIngresar.Location = new System.Drawing.Point(87, 91);
+            this.btnIngresar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnIngresar.Location = new System.Drawing.Point(119, 91);
             this.btnIngresar.Name = "btnIngresar";
             this.btnIngresar.Size = new System.Drawing.Size(75, 23);
             this.btnIngresar.TabIndex = 4;
@@ -80,27 +96,35 @@
             // 
             // btnSalir
             // 
-            this.btnSalir.Location = new System.Drawing.Point(168, 91);
+            this.btnSalir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSalir.Location = new System.Drawing.Point(200, 91);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(75, 23);
             this.btnSalir.TabIndex = 5;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(271, 128);
+            this.ClientSize = new System.Drawing.Size(324, 139);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnIngresar);
             this.Controls.Add(this.lblContraseña);
             this.Controls.Add(this.lblUsuario);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txbContraseña);
+            this.Controls.Add(this.txbUsuario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ingrese Usuario y Contraseña";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -108,12 +132,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txbUsuario;
+        private System.Windows.Forms.TextBox txbContraseña;
         private System.Windows.Forms.Label lblUsuario;
         private System.Windows.Forms.Label lblContraseña;
         private System.Windows.Forms.Button btnIngresar;
         private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
 
--- obligatorioPuntoNet2016Licencias/Login/Login.cs
+++ obligatorioPuntoNet2016Licencias/Login/Login.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Fomularios;
+using System.Configuration;
 
 namespace Login
 {
@@ -20,8 +21,68 @@ namespace Login
 
         private void btnIngresar_Click(object sender, EventArgs e)
         {
-            Fomularios.Configuraciones ventana = new Fomularios.Configuraciones();
-            ventana.Show();
+            if (validarUsuario(sender)) {
+                if (validarContraseña(sender)) {
+                    String usuario = LeerConfiguracion("usuario");
+                    String contraseña = LeerConfiguracion("contraseña");
+                    if (this.txbUsuario.Text.ToString() == usuario && this.txbContraseña.Text.ToString() == contraseña)
+                    {
+                        Fomularios.Configuraciones ventana = new Fomularios.Configuraciones();
+                        this.Visible = false;
+                        ventana.Show();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario o contraseña inválida.");
+                    }
+                }
+            }
+
+        }
+        private bool validarUsuario(Object sender) {
+            bool retorno = false;
+            if (String.IsNullOrEmpty(txbUsuario.Text))
+            {
+                errorProvider1.SetError(txbUsuario, "El usuario no puede ser vacío.");
+            }
+            else
+            {
+                errorProvider1.SetError(txbUsuario, String.Empty);
+                retorno = true;
+            }
+            return retorno;
+        }
+        private bool validarContraseña(Object sender)
+        {
+            bool retorno = false;
+            if (String.IsNullOrEmpty(txbContraseña.Text))
+                errorProvider1.SetError(txbContraseña, "La contraseña no puede ser vacía.");
+            else {
+                errorProvider1.SetError(txbUsuario, String.Empty);
+                retorno = true;
+            }
+            return retorno;
+        }
+
+        static String LeerConfiguracion(string key)
+        {
+            string result = "";
+            try
+            {
+                var appSettings = ConfigurationManager.AppSettings;
+                result = appSettings[key] ?? "No se encontro";
+                Console.WriteLine(result);
+            }
+            catch (ConfigurationErrorsException)
+            {
+                Console.WriteLine("Error Leyendo App.config");
+            }
+            return result;
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Dispose();
         }
     }
 }
